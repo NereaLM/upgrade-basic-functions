e2d8f93d256d0c53db8ec80379d83c20eedb4bbf
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2021-12-08 23:36:14 +0100

    seventh commit

--- script.js
+++ script.js
@@ -114,3 +114,29 @@ function removeDuplicates(param) {
 
 console.log(removeDuplicates(duplicates));
 
+
+//ITERACION#7: BUSCADOR DE NOMBRES
+
+// Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
+
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+function finderName(array , palabra) {    
+  return array.includes(palabra) ? array.indexOf(palabra) : false ;
+}
+
+console.log(finderName(nameFinder,"Tony"));
+console.log(finderName(nameFinder,"casa"));
+
+
