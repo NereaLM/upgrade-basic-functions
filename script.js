0ef2dea9a4c382587bd9b61a7f261fc42740f6db
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2021-12-08 23:34:48 +0100

    sixth commit

--- script.js
+++ script.js
@@ -85,3 +85,32 @@ function averageWord(param) {
 }
 let resultado = averageWord(mixedElements)
 console.log(resultado)
+
+
+
+//Iteración #6: Valores únicoS
+
+//Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
+
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
+function removeDuplicates(param) {
+    return param.sort((a,b) =>
+    param.filter(v => v===a).length
+     - param.filter(v => v===b).length
+  ).pop();
+}
+
+console.log(removeDuplicates(duplicates));
+
