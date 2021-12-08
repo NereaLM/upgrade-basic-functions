36bf00d86615c54a354da72cd3b51dfa44a18da9
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2021-12-08 23:27:54 +0100

    second commit

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ function sum(numberOne , numberTwo) {
 console.log(sum(654555874,88488));
 
 
+ //ITERACIÓN#2: BUSCAR LA PALABRA MÁS LARGA
+  
+  //Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+  //Puedes usar este array para probar tu función:
+
+  const avengers = [
+    'Hulk',
+    'Thor', 
+    'IronMan', 
+    'Captain A.', 
+    'Spiderman', 
+    'Captain M.'
+];
+function findLongestWord(param) {
+  let longest = param[0]
+  param.forEach((avengers) =>{
+      let longtitudMasLargo = longest.length
+      let longtitudValorActual = avengers.length
+      if(longtitudValorActual > longtitudMasLargo) {
+          longest = avengers
+      }
+  })
+  return longest
+}
+
+let resultado = findLongestWord(avengers)
+ console.log(resultado)   
