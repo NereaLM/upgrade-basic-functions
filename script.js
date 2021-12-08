ca617b18cf74f7477f305f91e9a0c2298da2f583
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2021-12-08 23:33:27 +0100

    fifth commit

--- script.js
+++ script.js
@@ -63,3 +63,25 @@ function average(param) {
      return promedio;
 }
  console.log(average(numbers2));
+
+
+ //ITERACION#5: CALCULAR PROMEDIO DE STRING
+//Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:
+// Array.isArray()
+// let miArray = []
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+function averageWord(param) {
+    let total = 0
+  for(let i = 0; i < param.length; i++) {
+      debugger
+      if (typeof param[i] ==="number") {
+          total += param[i]
+      } else {
+          total += param[i].length
+      }
+  } 
+   return total
+}
+let resultado = averageWord(mixedElements)
+console.log(resultado)
