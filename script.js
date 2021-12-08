97539c33edb8626fb387ebfe2dec103afe6c1125
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2021-12-08 23:25:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//ITERACIÓN#1: BUSCAR EL MÁXIMO
+
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+   if(numberOne < numberTwo){
+       return numberTwo;
+   }else{
+       return numberOne;
+   }
+}
+console.log(sum(654555874,88488));
+
+
